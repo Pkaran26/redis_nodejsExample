33dad67389f447961a21501368e8d65f631bd09a
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2024-05-03 22:17:49 +0530

    Update acronis-linux.py

--- acronis-linux.py
+++ acronis-linux.py
@@ -34,7 +34,7 @@ def getInstanceID():
 
 def installAgent ():
   file_name = "CyberProtect_AgentForLinux_x86_64.bin"
-  file_url = "https://tnow-prod-apac.367791ca7abea81096902b345fee7b1f.r2.cloudflarestorage.com/2024-05-03/87879b363731b324031ce228b2940c5e/20240503Aoo3M7fn/O5WXGX/CyberProtect_AgentForLinux_x86_64%20%281%29.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ee862debb448801ab1904792186e2774%2F20240503%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20240503T135908Z&X-Amz-Expires=86400&X-Amz-Signature=9668793661941e8bcdb109aa7b0ce2e44dc890f7e86d46cb87c65475cf758e01&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22CyberProtect_AgentForLinux_x86_64%20%281%29.bin%22&x-id=GetObject"
+  file_url = "https://tnow-prod-apac.367791ca7abea81096902b345fee7b1f.r2.cloudflarestorage.com/2024-05-03/87879b363731b324031ce228b2940c5e/20240503Aoo3M7fn/O5WXGX/CyberProtect_AgentForLinux_x86_64%20%281%29.bin?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ee862debb448801ab1904792186e2774%2F20240503%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20240503T164623Z&X-Amz-Expires=86400&X-Amz-Signature=3df62c85083066bc7da9e3c4b9d50fe05790d20afd8d55ec6f773352b9959efe&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22CyberProtect_AgentForLinux_x86_64%20%281%29.bin%22&x-id=GetObject"
 
   r = requests.get(file_url, stream = True) 
 
@@ -45,7 +45,7 @@ def installAgent ():
   print('Downloaded: {0:.2f} MB'.format(os.path.getsize(file_name) * 0.000001), end='\n')
   print(file_name + ' is installing...')
   print(os.system("sudo chmod +x ./" + file_name))
-  print(os.system("sudo ./" + file_name + " --skip-registration"))
+  print(os.system("sudo ./" + file_name + "-a --skip-registration"))
 
   InstanceID = getInstanceID()
   print("InstanceID: ", InstanceID)
