55f67a3d17e45b4fe98a4e76715f13cb008c5d51
Prateek Kaushik <39780074+Pkaran26@users.noreply.github.com>
2024-05-10 12:36:42 +0530

    Update acronis-linux.py

--- acronis-linux.py
+++ acronis-linux.py
@@ -34,15 +34,15 @@ def getInstanceID():
 
 def installAgent ():
   file_name = "CyberProtect_AgentForLinux_x86_64.bin"
-  file_url = "https://download.wetransfer.com/eugv/c824a8fffc2a34007f8d3ac422786b0320240508124056/bda88f1f5e29bb8974b04736fe743235008a02f9/CyberProtect_AgentForLinux_x86_64%20%281%29.bin?cf=y&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRlZmF1bHQifQ.eyJleHAiOjE3MTUxNzI4MjIsImlhdCI6MTcxNTE3MjIyMiwiZG93bmxvYWRfaWQiOiIwYmNjMTA2NC0xNTg1LTQ4Y2UtYjhmMi1hNzQ5OWU1NzJlM2UiLCJzdG9yYWdlX3NlcnZpY2UiOiJzdG9ybSJ9.RILUsw09kvTlXogwokm4nUm9HD9R4rP7Zq09XgyIHoc"
+  #file_url = "https://download.wetransfer.com/eugv/c824a8fffc2a34007f8d3ac422786b0320240508124056/bda88f1f5e29bb8974b04736fe743235008a02f9/CyberProtect_AgentForLinux_x86_64%20%281%29.bin?cf=y&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRlZmF1bHQifQ.eyJleHAiOjE3MTUxNzI4MjIsImlhdCI6MTcxNTE3MjIyMiwiZG93bmxvYWRfaWQiOiIwYmNjMTA2NC0xNTg1LTQ4Y2UtYjhmMi1hNzQ5OWU1NzJlM2UiLCJzdG9yYWdlX3NlcnZpY2UiOiJzdG9ybSJ9.RILUsw09kvTlXogwokm4nUm9HD9R4rP7Zq09XgyIHoc"
 
-  r = requests.get(file_url, stream = True) 
+  #r = requests.get(file_url, stream = True) 
 
-  with open(file_name,"wb") as file: 
-    for chunk in r.iter_content(chunk_size=1024): 
-      if chunk: 
-        file.write(chunk);print('Downloaded: {0:.2f} MB'.format(os.path.getsize(file_name) * 0.000001), end='\r')
-  print('Downloaded: {0:.2f} MB'.format(os.path.getsize(file_name) * 0.000001), end='\n')
+  #with open(file_name,"wb") as file: 
+    #for chunk in r.iter_content(chunk_size=1024): 
+      #if chunk: 
+       # file.write(chunk);print('Downloaded: {0:.2f} MB'.format(os.path.getsize(file_name) * 0.000001), end='\r')
+  #print('Downloaded: {0:.2f} MB'.format(os.path.getsize(file_name) * 0.000001), end='\n')
   print(file_name + ' is installing...')
   print(os.system("sudo chmod +x ./" + file_name))
   print(os.system("sudo ./" + file_name + " -a --skip-registration"))
